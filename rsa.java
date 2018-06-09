a880188840015cb37bfdbbe1c6d4bc312839d564
besnikkelmendi <35761060+besnikkelmendi@users.noreply.github.com>
2018-06-09 16:02:28 +0200

    Update rsa.java

--- rsa.java
+++ rsa.java
@@ -64,3 +64,11 @@ public rsa()
         p = BigInteger.probablePrime(bitlength, r);
         q = BigInteger.probablePrime(bitlength, r);
         N = p.multiply(q);
+        phi = p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE));
+        e = BigInteger.probablePrime(bitlength / 2, r);
+        while (phi.gcd(e).compareTo(BigInteger.ONE) > 0 && e.compareTo(phi) < 0)
+        {
+            e.add(BigInteger.ONE);
+        }
+        d = e.modInverse(phi);    
+    }
