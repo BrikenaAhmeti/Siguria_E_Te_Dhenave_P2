405407b6c60e88762e0cd844a93f7193e7c4aff8
Brikena Ahmeti <kena.97@hotmail.com>
2018-06-09 15:59:43 +0200

    Update rsa.java

--- rsa.java
+++ rsa.java
@@ -30,3 +30,25 @@ int gcd(int a, int h)
         h = temp;
     }
 }
+int modInverse(int a, int m)
+    {
+    	//a perfaqeson e ,ndersa m phi
+        int m0 = m;
+        int y = 0, x = 1;
+ 
+        if (m == 1)
+            return 0;
+ 
+        while (a > 1)
+        {          
+            int q = a / m;
+            int t = m;
+ 
+          
+            m = a % m;
+            a = t;
+            t = y;
+ 
+            y = x - q * y;
+            x = t;
+        }
